4e6a08b27acba34e9dc17b8d72ee21d527c63a0a
Waseem Anjum <waseemaofficial@gmail.com>
2025-01-11 15:31:24 +0500

    seo updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,31 @@ export const metadata: Metadata = {
   title: "GTA VI Countdown Timer",
   description:
     "Track the countdown to GTA VI release and days since first trailer",
+  keywords: [
+    "GTA VI",
+    "GTA",
+    "GTA 6",
+    "GTA 6 countdown",
+    "GTA 6 release",
+    "GTA 6 trailer",
+    "GTA 6 release date",
+    "GTA 6 release date countdown",
+    "GTA 6 release date countdown timer",
+  ],
+  openGraph: {
+    title: "GTA VI Countdown Timer",
+    description:
+      "Track the countdown to GTA VI release and days since first trailer",
+    url: "https://gta-vi-countdown.vercel.app",
+    siteName: "GTA VI Countdown Timer",
+  },
+  applicationName: "GTA VI Countdown Timer",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  creator: "https://github.com/fabwaseem",
+  category: "games",
 };
 
 export default function RootLayout({
